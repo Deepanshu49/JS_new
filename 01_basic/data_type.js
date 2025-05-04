1604df9f5ff01e53ce2f44e183b23693b19ae201
Deepanshu49 <codexdeepanshu@gmail.com>
2025-05-04 03:05:57 +0000

    container config done

---
+++ 01_basic/data_type.js
@@ -0,0 +1,3 @@
+"use strict "; // treat all JS code as newer version
+//alert(2+1) // we are using node.js not browser
+console.log(3+3) ;console.log("hsehe")//write in a way that it is readble for other 
