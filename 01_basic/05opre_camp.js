e74b337fd6b2cdbbe7566ba1f22034ade420880e
Deepanshu49 <codexdeepanshu@gmail.com>
2025-05-04 06:40:28 +0000

    05opre_camp.js

---
+++ 01_basic/05opre_camp.js
@@ -0,0 +1,16 @@
+// Oeration as same as other language jo diff karega wo niche hai
+var hell = 212
+hell++
+console.log(hell)
+// lagabag sab tho same hi hai
+//comparison 
+// always compare same datatype  
+console.log(null >0)
+console.log(null == 0)
+console.log(null >= 0)
+// on camparision operation null is treated as num = 0 
+console.log(undefined >0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+// === is strictly check it also compare datatype 
+// avoid these type if code always write clen code 
