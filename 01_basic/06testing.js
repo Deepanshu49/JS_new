1b432462130b8cd055135cef8a527120667d0d1f
deepanshu <codexdeepanshu@gmail.com>
2025-05-04 17:50:48 +0530

    Just testing for push

--- 01_basic/06testing.js
+++ 01_basic/06testing.js
@@ -1,2 +1,17 @@
 console.log("Hello Kya haal chal ")
 console.log("This is Deepanshu Shukla From United")
+//Pull from github :
+/*
+s1 : copy url of your repo 
+s2 : type git clone in your terminal 
+s3 : paste the copied url
+s4 : cd your_repo
+*/
+//Push to github 
+/*
+s1 : create a file that should be uploaded 
+s2 : open terminal
+s3 : git add .
+s4 : git commit -m :"Add file name "
+s5 : git push --> Done 
+*/
