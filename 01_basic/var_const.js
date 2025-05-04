263a1c9f2f3116ac89705531e50c37ed81a145c5
Deepanshu49 <codexdeepanshu@gmail.com>
2025-05-04 02:55:24 +0000

    container config done

---
+++ 01_basic/var_const.js
@@ -0,0 +1,13 @@
+const account_Id = 409192
+let account_Email = "codex.deepanshu"
+var account_Password = "aflafk"
+hello = "Hello"
+var account_kuch_bi;
+console.log(account_Email);
+console.log(account_Id);
+account_Email = "fnewfnofief"
+console.log(account_Email);
+console.table([account_Id,account_Email,account_Password,hello,account_kuch_bi]);
+/* 
+var ko mat use karo because it is not bolck and functional scope  orenited .
+*/
