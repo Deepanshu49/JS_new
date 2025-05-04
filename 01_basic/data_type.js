dc12b038273278f821ef65c3e6593d4d6c20412a
Deepanshu49 <codexdeepanshu@gmail.com>
2025-05-04 03:12:34 +0000

    data_types

--- 01_basic/data_type.js
+++ 01_basic/data_type.js
@@ -1,3 +1,11 @@
 "use strict "; // treat all JS code as newer version
 //alert(2+1) // we are using node.js not browser
 console.log(3+3) ;console.log("hsehe")//write in a way that it is readble for other 
+//Data type
+/* string (single or double '' or "") , number = (2^23) around  , boolean , bigint ,mull ,undefine ;  
+symbol => unique 
+object 
+typeof() to check the type of the data 
+null ka type object 
+undefine ka type bi undefine hi hai 
+*/
