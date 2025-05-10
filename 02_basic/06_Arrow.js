e7f566696696290c7ba39db078751f0c6cbe6a63
Deepanshu Shukla <codexdeepanshu@gmail.com>
2025-05-10 19:25:01 +0530

    Arrow funtion done

---
+++ 02_basic/06_Arrow.js
@@ -0,0 +1,31 @@
+// to refere the current conntext we use the arrow function -> and this. 
+//the result of the this in the object is the object within it call and outsite of the object it result ti empty object 
+
+//for the function
+
+// function add(){
+//     let use = "Deepanshu"
+//     console.log(this.use)
+    
+// }
+// add()
+
+// const chai = function(){
+//     let sss = "Deepu"
+//     console.log(this.sss)
+// }
+// chai()
+//that means this is not use in  function as it is used for object  
+// Same for both type of declare function 
+
+//to use the this we use arrow funtion
+
+const chai = () => {//this is declaration of the arrow function
+    let sss = "Deepu"
+    console.log(this)
+}
+chai()
+//Arrow function
+//declare () =>
+//Implisite arrow funtion
+//var_Type var_name = (parameter) => (what_you_want_to_return) //for the function of one line 
